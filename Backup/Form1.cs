73477053ca87e466bef2737bf83d023112701509
jash-git <jash.liao@gmail.com>
2018-07-06 13:39:47 +0800

    https://github.com/jash-git/CS_TabControl_Color.git

---
+++ Backup/Form1.cs
@@ -0,0 +1,170 @@
+using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Data;
+
+namespace SelectedTabPage
+{
+	/// <summary>
+	/// Summary description for Form1.
+	/// </summary>
+	public class Form1 : System.Windows.Forms.Form
+	{
+		private System.Windows.Forms.TabControl tabControl1;
+		private System.Windows.Forms.TabPage tabPage1;
+		private System.Windows.Forms.TabPage tabPage2;
+		private System.Windows.Forms.TabPage tabPage3;
+		private System.Windows.Forms.TabPage tabPage4;
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.Container components = null;
+
+		public Form1()
+		{
+			//
+			// Required for Windows Form Designer support
+			//
+			InitializeComponent();
+			this.tabControl1.DrawMode = System.Windows.Forms.TabDrawMode.OwnerDrawFixed;
+			this.tabControl1.DrawItem += new DrawItemEventHandler(this.tabControl1_DrawItem);
+			//
+			// TODO: Add any constructor code after InitializeComponent call
+			//
+		}
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		protected override void Dispose( bool disposing )
+		{
+			if( disposing )
+			{
+				if (components != null) 
+				{
+					components.Dispose();
+				}
+			}
+			base.Dispose( disposing );
+		}
+
+		#region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.tabControl1 = new System.Windows.Forms.TabControl();
+			this.tabPage1 = new System.Windows.Forms.TabPage();
+			this.tabPage2 = new System.Windows.Forms.TabPage();
+			this.tabPage3 = new System.Windows.Forms.TabPage();
+			this.tabPage4 = new System.Windows.Forms.TabPage();
+			this.tabControl1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// tabControl1
+			// 
+			this.tabControl1.Controls.Add(this.tabPage1);
+			this.tabControl1.Controls.Add(this.tabPage2);
+			this.tabControl1.Controls.Add(this.tabPage3);
+			this.tabControl1.Controls.Add(this.tabPage4);
+			this.tabControl1.Location = new System.Drawing.Point(16, 24);
+			this.tabControl1.Name = "tabControl1";
+			this.tabControl1.SelectedIndex = 0;
+			this.tabControl1.Size = new System.Drawing.Size(312, 208);
+			this.tabControl1.TabIndex = 0;
+			// 
+			// tabPage1
+			// 
+			this.tabPage1.Location = new System.Drawing.Point(4, 22);
+			this.tabPage1.Name = "tabPage1";
+			this.tabPage1.Size = new System.Drawing.Size(304, 182);
+			this.tabPage1.TabIndex = 0;
+			this.tabPage1.Text = "User";
+			// 
+			// tabPage2
+			// 
+			this.tabPage2.Location = new System.Drawing.Point(4, 22);
+			this.tabPage2.Name = "tabPage2";
+			this.tabPage2.Size = new System.Drawing.Size(304, 182);
+			this.tabPage2.TabIndex = 1;
+			this.tabPage2.Text = "Role";
+			// 
+			// tabPage3
+			// 
+			this.tabPage3.Location = new System.Drawing.Point(4, 22);
+			this.tabPage3.Name = "tabPage3";
+			this.tabPage3.Size = new System.Drawing.Size(304, 182);
+			this.tabPage3.TabIndex = 2;
+			this.tabPage3.Text = "Screen";
+			// 
+			// tabPage4
+			// 
+			this.tabPage4.Location = new System.Drawing.Point(4, 22);
+			this.tabPage4.Name = "tabPage4";
+			this.tabPage4.Size = new System.Drawing.Size(304, 182);
+			this.tabPage4.TabIndex = 3;
+			this.tabPage4.Text = "Module";
+			// 
+			// Form1
+			// 
+			this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
+			this.ClientSize = new System.Drawing.Size(360, 261);
+			this.Controls.Add(this.tabControl1);
+			this.Name = "Form1";
+			this.Text = "Selected Tab Page";
+			this.tabControl1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+		#endregion
+
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main() 
+		{
+			Application.Run(new Form1());
+		}
+
+
+		private void tabControl1_DrawItem(object sender, System.Windows.Forms.DrawItemEventArgs e)
+		{
+			Font fntTab;
+			Brush bshBack;
+			Brush bshFore;
+
+			if ( e.Index == this.tabControl1.Selecte
+				dIndex)
+			{
+				fntTab = new Font(e.Font, FontStyle.Bold);
+				bshBack = new System.Drawing.Drawing2D.LinearGradientBrush(e.Bounds, SystemColors.Control, SystemColors.Control, System.Drawing.Drawing2D.LinearGradientMode.BackwardDiagonal);
+				bshFore = Brushes.Black;
+				//bshBack = new System.Drawing.Drawing2D.LinearGradientBrush(e.Bounds, Color.LightSkyBlue , Color.LightGreen, System.Drawing.Drawing2D.LinearGradientMode.BackwardDiagonal);
+				//bshFore = Brushes.Blue;
+			}
+			else
+			{
+				fntTab = e.Font;
+				bshBack = new SolidBrush(SystemColors.Control);
+				bshFore = new SolidBrush(Color.Black);
+
+				//bshBack = new SolidBrush(Color.White);
+				//bshFore = new SolidBrush(Color.Black);
+			}
+     
+			string tabName  = this.tabControl1.TabPages[e.Index].Text;
+			StringFormat sftTab = new StringFormat();
+			e.Graphics.FillRectangle(bshBack, e.Bounds);
+			Rectangle  recTab = e.Bounds;
+			recTab = new Rectangle( recTab.X,  recTab.Y + 4,  recTab.Width,  recTab.Height - 4);
+			e.Graphics.DrawString(tabName, fntTab, bshFore, recTab, sftTab);
+
+		}
+
+	}
+}
